c09172adc84e8753278fc32db95777f1bf63d4c4
Taylor Downs <taylor@openfn.org>
2019-12-11 20:18:11 +0000

    sconrad@digitalimpactalliance.org's change on OpenFn.org

--- jobs/02a-query-people-needing-payment.js
+++ jobs/02a-query-people-needing-payment.js
@@ -1,11 +1,25 @@
 sqlString(state => {
-  // select all current employees
-  return 'SELECT id, parent, start_date FROM hippo_person_position WHERE DATE(`start_date`) < CURDATE()';
+   return 'SELECT id, form FROM form_field WHERE form=(select id from form where name=`person_payments`) and field=(select id from field where name=`date`)'
+});
+
+alterState(state => {
+  state.data.payment_date_field = state.response.body[0]
+});
+
+SELECT MAX(date_value) FROM entry WHERE record in (SELECT id)
+sqlString(state => {
+  // select all current employees who have been working for at least one week
+  return 'SELECT id, parent, position FROM hippo_person_position WHERE start_date < DATE(DATE_SUB(NOW(), INTERVAL 7 DAY))';
 });
 
 alterState(state => {
   state.data.curr_employees = state.response.body.filter(x => x.id !== undefined);
 });
+
+state.data.curr_employees && state.data.curr_employees.map(employee => {
+  // get last payment date for this employee
+  sqlString
+})
 
 sqlString(state => {
   // select person_position records
