bb8cc9b6d1350832e2a4709e2072febf2c32a28d
Taylor Downs <taylor@openfn.org>
2019-12-11 11:57:18 +0000

    taylor@openfn.org's change on OpenFn.org

--- jobs/02a-query-people-needing-payment.js
+++ jobs/02a-query-people-needing-payment.js
@@ -1,9 +1,10 @@
 sqlString(state => {
-  return `SELECT name, start_date, salary FROM hippo_person_position
-    WHERE start_date = ${new Date()} and age(last_payment_date) > '30 days'`;
+  return 'SELECT id, start_date FROM hippo_person_position';
+  // + `WHERE start_date = '${new Date().toString()}' and age(last_payment_date) > '30 days'`
 });
 
 alterState(state => {
-  console.log(state.references);
+  // Note: we pluck out on the 'RowDataResponses' from the SQL server.
+  state.records = state.response.body.filter(x => x.id !== undefined);
   return state;
 });
