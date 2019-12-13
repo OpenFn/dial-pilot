37b6ac0c3ae3bddeadc4b2ba57f9900e9600e75d
Taylor Downs <downs.taylor@gmail.com>
2019-12-13 07:26:31 -0500

    filter on position, stop after returning an array for payment

--- jobs/04a-query-for-job-changes.js
+++ jobs/04a-query-for-job-changes.js
@@ -1,15 +1,20 @@
-sqlString(state => {
-  // stored procedure will return a list of names, phone numbers (MSISDN), and salary info for employees that should be paid today
-  return `SELECT position, facility, status FROM position_updates WHERE status='NEW'`;
-});
+sqlString(
+  state =>
+    "SELECT position, facility, status FROM position_updates WHERE status='NEW'"
+);
 
 alterState(state => {
+  console.log(state.response.body);
+
   // Note: we pluck out on the 'RowDataResponses' from the SQL server.
-  state.data.open_positions = state.response.body.filter(x => x.id !== undefined);
+  state.data.open_positions = state.response.body.filter(
+    x => x.position !== undefined
+  );
   return state;
 });
 
-sqlString(state => {
-  // stored procedure will return a list of names, phone numbers (MSISDN), and salary info for employees that should be paid today
-  return `UPDATE position_updates SET status='PROCESSED' WHERE status='NEW'`;
-});
+// QUESTION: Should we wait to do this update until after we've done things in Mifos?
+// sqlString(state => {
+//   // stored procedure will return a list of names, phone numbers (MSISDN), and salary info for employees that should be paid today
+//   return `UPDATE position_updates SET status='PROCESSED' WHERE status='NEW'`;
+// });
