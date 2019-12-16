5c8dcd3bd92d8249826c903b6a969a60055a1378
Taylor Downs <taylor@openfn.org>
2019-12-16 11:49:40 -0500

    nribeka@digitalimpactalliance.org's change on OpenFn.org

--- jobs/04-update-odk-form.js
+++ jobs/04-update-odk-form.js
@@ -1,6 +1,8 @@
 // Your job goes here.
+console.log('State:', state);
 get('http://167.71.88.252/formXml?formId=registrion_form', {
   callback: function (state) {
+    console.log('Returned state:', state);
     const template = state.data.data;
 
     const formData = new FormData();
