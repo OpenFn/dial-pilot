6d34067e22f3292b093c1718660a8922c24792c8
Taylor Downs <taylor@openfn.org>
2019-12-16 18:33:32 -0500

    nribeka@digitalimpactalliance.org's change on OpenFn.org

--- jobs/04-update-odk-form.js
+++ jobs/04-update-odk-form.js
@@ -9,11 +9,6 @@ get(
   },
   function (state) {
     let template = state.data.body;
-    
-    let boundary = '--------------------------';
-    for (var i = 0; i < 24; i++) {
-      boundary += Math.floor(Math.random() * 10).toString(16);
-    }
 
     const versionEx = /id="\S+"\s+version="(\S+)"/;
     const matches = template.match(versionEx);
@@ -21,24 +16,16 @@ get(
     const currentVersion = Number.parseInt(matches[1]);
     template = template.replace(currentVersion, currentVersion + 1);
 
-    alterState(state => {
-      state.template = template;
-      return state;
-    });
-  }
-);
-
-console.log('Template:');
-console.log(state.template);
-
-post(
-  'http://167.71.88.252/formUpload',
-  {
-    formData: {
-      form_def_file: state.template
-    }
-  },
-  function(state) {
-    console.log(state);
+    post(
+      'http://167.71.88.252/formUpload',
+      {
+        formData: {
+          form_def_file: state.template
+        }
+      },
+      function(state) {
+        console.log(state);
+      }
+    );
   }
 );
