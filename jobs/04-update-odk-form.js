9c443525f59c297f4f74519a7cfb117b04c8d673
Taylor Downs <taylor@openfn.org>
2019-12-16 17:33:52 -0500

    nribeka@digitalimpactalliance.org's change on OpenFn.org

--- jobs/04-update-odk-form.js
+++ jobs/04-update-odk-form.js
@@ -1,30 +1,30 @@
 // Your job goes here.
 console.log('State:', state.response.body);
-get('http://167.71.88.252/formXml?formId=registrion_form',
-  {},
-  function (state) {
-    console.log('Returned state:', state.response.body);
-    let template = state.response.body.toString();
-    
-    let boundary = '--------------------------';
-    for (var i = 0; i < 24; i++) {
-      boundary += Math.floor(Math.random() * 10).toString(16);
-    }
-
-    let indexOfVersion = template.lastIndexOf('version');
-    // find the version code.
-    console.log(indexOfVersion);
-
-    post('http://167.71.88.252/formUpload', {
-      headers: {
-        'content-type': 'multipart/form-data; boundary=' + boundary
-      },
-      formData: {
-        'form_def_file': template
-      },
-      options: {
-        successCodes: [201],
+get('http://167.71.88.252/formXml?formId=registrion_form')
+  .then(
+    function (state) {
+      console.log('Returned state:', state.response.body);
+      let template = state.response.body.toString();
+      
+      let boundary = '--------------------------';
+      for (var i = 0; i < 24; i++) {
+        boundary += Math.floor(Math.random() * 10).toString(16);
       }
-    });
-  }
-);
+  
+      let indexOfVersion = template.lastIndexOf('version');
+      // find the version code.
+      console.log(indexOfVersion);
+  
+      post('http://167.71.88.252/formUpload', {
+        headers: {
+          'content-type': 'multipart/form-data; boundary=' + boundary
+        },
+        formData: {
+          'form_def_file': template
+        },
+        options: {
+          successCodes: [201],
+        }
+      });
+    }
+  );
