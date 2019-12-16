3c4017256109e096f2e7de668b6e97de1875eb52
Taylor Downs <taylor@openfn.org>
2019-12-16 11:48:18 -0500

    nribeka@digitalimpactalliance.org's change on OpenFn.org

--- jobs/04-update-odk-form.js
+++ jobs/04-update-odk-form.js
@@ -2,33 +2,9 @@
 get('http://167.71.88.252/formXml?formId=registrion_form', {
   callback: function (state) {
     const template = state.data.data;
-    const $ = cheerio.load(template, { xmlMode: true });
-
-    // This part to remove a position.
-    const positionSelect = $('select1[ref="/RegistrationForm/position"]');
-    if (positionSelect.text().indexOf('10389-100000') >= 0) {
-      $('item', positionSelect).each((_item, element) => {
-        console.log(`Processing item: ${$(element).html()}`);
-        if ($(element).text().indexOf('10389-100000') >= 0) {
-          const removedItem = $(element).remove();
-          console.log(`Removed item: ${$(removedItem).html()}`);
-        }
-      });
-    }
-
-    // This part to add a position.
-    positionSelect.append(
-      `<item>
-             <label>Social Worker</label>
-             <value>10389-100000</value>
-           </item>`
-    );
-
-    const currentVersion = $('RegistrationForm').attr('version');
-    $('RegistrationForm').attr('version', parseInt(currentVersion) + 1);
 
     const formData = new FormData();
-    formData.append('form_def_file', $.html(), 'registration_form.xml');
+    formData.append('form_def_file', template, 'registration_form.xml');
 
     post('http://167.71.88.252/formUpload', {
       headers: formData.getHeaders(),
