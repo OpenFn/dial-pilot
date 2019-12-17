7043344d5b2472a9155b7b2608e96b9523f816a0
Taylor Downs <taylor@openfn.org>
2019-12-17 12:32:29 -0500

    sconrad@digitalimpactalliance.org's change on OpenFn.org

--- jobs/01c-create-person-position-record.js
+++ jobs/01c-create-person-position-record.js
@@ -11,7 +11,8 @@ alterState(state => {
     'form[person_position][0][0][fields][start_date][day]': startDateComponents[2],
     'form[person_position][0][0][fields][start_date][month]': startDateComponents[1],
     'form[person_position][0][0][fields][start_date][year]': startDateComponents[0],
-    'form[salary][0][0][fields][salary]': 'currency|3='+positionData[1]
+    'form[salary][0][0][fields][salary]': 'currency|3='+positionData[1],
+    'can_edit_position': '1'
   };
   state.person_contact = {
     'form[person_contact_personal][0][0][fields][id]': 'person_contact_personal|0',
@@ -30,7 +31,6 @@ post(
   {
     formData: state => {
       state.person_position.submit_type = 'confirm';
-      state.person_position.can_edit_position = '1';
       return state.person_position;
     },
   },
