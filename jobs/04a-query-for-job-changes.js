59a986b55dc58a3069ae4f451942754abb686502
Taylor Downs <taylor@openfn.org>
2019-12-17 11:09:38 -0500

    nribeka@digitalimpactalliance.org's change on OpenFn.org

--- jobs/04a-query-for-job-changes.js
+++ jobs/04a-query-for-job-changes.js
@@ -1,5 +1,5 @@
 sqlString(state => {
-  return "SELECT position, salary, facility_id, facility_name, status FROM position_updates WHERE status='NEW';";
+  return "SELECT position_id, position_name, salary, facility_id, facility_name, status FROM position_updates WHERE status='NEW';";
 });
 
 // alterState(state => {
