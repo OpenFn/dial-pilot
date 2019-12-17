1853f2f67adcbc7d975997d1891cb03640ab7d7f
Taylor Downs <taylor@openfn.org>
2019-12-17 12:47:54 -0500

    nribeka@digitalimpactalliance.org's change on OpenFn.org

--- jobs/04b-update-odk-form.js
+++ jobs/04b-update-odk-form.js
@@ -9,23 +9,76 @@ get(
   },
   state => {
     let template = state.data.body;
+    let templateUpdated = false;
 
-    const positionEx = /<item>\s+<label>[^<>]*<\/label>\s+<value>[^<>]*<\/value>\s+<\/item>/gi;
-    const positionMatches = template.match(positionEx);
-    template = template.replace(positionEx, '');
+    const itemEx = /<item>\s?<label>[^<>]*<\/label>\s?<value>[^<>]*<\/value>\s?<\/item>/gi;
+    const itemMatches = template.match(itemEx);
 
-    const selectEx = /<select1\s+ref="\/RegistrationForm\/position">/gi;
-    const selectMatches = template.match(selectEx);
-    template = template.replace(
-      selectEx,
-      selectMatches[0] + positionMatches[0]
-    );
+    const selectFacilityEx = /<select1\s+ref="\/RegistrationForm\/position_facility">/gi;
+    const selectPositionEx = /<select1\s+ref="\/RegistrationForm\/position">/gi;
 
-    const versionEx = /id="\S+"\s+version="(\S+)"/;
-    const versionMatches = template.match(versionEx);
 
-    const currentVersion = Number.parseInt(versionMatches[1]);
-    state.template = template.replace(currentVersion, currentVersion + 1);
+    for(let j = 0; j < state.response.body; j ++) {
+      let facilityExists = false;
+      let positionExists = false;
+
+      let facilityId = state.response.body[j].facility_id;
+
+      const positions = state.response.body[j].position.split('|'); 
+      const salaries = state.response.body[j].salary.split('=');
+
+      let positionId = `${positions[1]}_${salaries[1]}`;
+      for(let i = 0; i < itemMatches.length; i ++) {
+        if (itemMatches[i].indexOf(facilityId) >= 0) {
+          facilityExists = true;
+        }
+        if (itemMatches[i].indexOf(positionId) >= 0) {
+          positionExists = true;
+        }
+      }
+
+      if (!facilityExists) {
+        const selectFacilityMatches = template.match(selectFacilityEx);
+        const facilityItem =
+          `<item>
+             <label>${state.response.body[j].facility_name}</label>
+             <value>${state.response.body[j].facility_id}</value>
+           </item>`;
+        template = template.replace(
+          selectFacilityEx,
+          selectFacilityMatches[0] + facilityItem
+        );
+        templateUpdated = true;
+      } else {
+        console.log('Not seeing any facility to add. Moving along!');
+      }
+
+      if (!positionExists) {
+        const selectPositionMatches = template.match(selectPositionEx);
+        const positionItem =
+          `<item>
+             <label>${state.response.body[j].position_name}</label>
+             <value>${position_id}</value>
+           </item>`;
+        template = template.replace(
+          selectPositionEx,
+          selectPositionMatches[0] + positionItem
+        );
+        templateUpdated = true;
+      } else {
+        console.log('Not seeing any position to add. Moving along!');
+      }
+    }
+
+    if (templateUpdated) {
+      const versionEx = /id="\S+"\s+version="(\S+)"/;
+      const versionMatches = template.match(versionEx);
+      const currentVersion = Number.parseInt(versionMatches[1]);
+      console.log(`Updating form version from: ${currentVersion}`);
+      template = template.replace(currentVersion, currentVersion + 1);
+    }
+
+    state.template = template;
     return state;
   }
 );
