807e2bdc0e402e018929809e6886a0b1b3a0d7d0
Taylor Downs <taylor@openfn.org>
2019-12-17 09:38:35 -0500

    nribeka@digitalimpactalliance.org's change on OpenFn.org

--- jobs/04b-update-odk-form.js
+++ jobs/04b-update-odk-form.js
@@ -7,6 +7,9 @@ get(
   },
   state => {
     let template = state.data.body;
+    
+    let re = new RegExp('id="\S+"\s+version="(\S+)"');
+    console.log(re);
 
     const versionEx = /id="\S+"\s+version="(\S+)"/;
     const versionMatches = template.match(versionEx);
