6a4a9db4e65e4b1c1c8dbc9254a97dc2d3ed1d0a
Taylor Downs <taylor@openfn.org>
2019-12-17 15:40:10 -0500

    sconrad@digitalimpactalliance.org's change on OpenFn.org

--- jobs/02b-initiate-payments.js
+++ jobs/02b-initiate-payments.js
@@ -55,7 +55,7 @@ each(
     },
     state => {
       const today=new Date();
-      console.log(state);
+      console.log(JSON.stringify(state.response.body));
       state.data.person_payments = {
         'form[person_payments][0][0][fields][id]': 'person_payments|0',
         'form[person_payments][0][0][fields][parent]': 'person|'+state.data.person_id,
