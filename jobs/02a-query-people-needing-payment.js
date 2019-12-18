2c8c2cbf43823ed8c2ece1135911efe4ab266756
Taylor Downs <taylor@openfn.org>
2019-12-18 11:43:24 -0500

    sconrad@digitalimpactalliance.org's change on OpenFn.org

--- jobs/02a-query-people-needing-payment.js
+++ jobs/02a-query-people-needing-payment.js
@@ -4,8 +4,8 @@ sqlString(state => {
   return 'CALL get_employees_to_pay();';
 });
 
-// alterState(state => {
-//   // Note: we pluck out on the 'RowDataResponses' from the SQL server.
-//   state.data.records = state.response.body.filter(x => x.id !== undefined);
-//   return state;
-// });
+alterState(state => {
+  // Note: we pluck out on the 'RowDataResponses' from the SQL server.
+  state.data.records = state.response.body.filter(x => x.id !== undefined);
+  return state;
+});
