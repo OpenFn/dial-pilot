9a2fae12833c6ff79dd2ce49fd39b543823be462
Taylor Downs <taylor@openfn.org>
2019-12-18 16:41:19 -0500

    sconrad@digitalimpactalliance.org's change on OpenFn.org

--- jobs/02b-initiate-payments.js
+++ jobs/02b-initiate-payments.js
@@ -17,9 +17,9 @@ alterState(state => {
 // Make payment requests in Mifos with the reponse, either in bulk or by
 // iterating through the array of registrants and making a separate request
 // for each one ==============================================================
-console.log(state.records)
+console.log(state.payees)
 each(
-  state.records,
+  state.payees,
   post(
     `${state.configuration.mifosUrl}/channel/transactions`,
     {
