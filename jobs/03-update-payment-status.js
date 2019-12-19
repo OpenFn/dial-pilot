32638ab3d091c661a0cc2a6d704796b4b215f1b7
Taylor Downs <taylor@openfn.org>
2019-12-19 11:53:42 -0500

    sconrad@digitalimpactalliance.org's change on OpenFn.org

--- jobs/03-update-payment-status.js
+++ jobs/03-update-payment-status.js
@@ -3,6 +3,14 @@ sqlString(state => {
   return `SELECT record FROM entry WHERE string_value='${state.data.transactionCode}'`;
 });
 
+alterState(state => {
+  // Note: we pluck out on the 'RowDataResponses' from the SQL server.
+  const record = state.response.body.filter(x => x.id !== undefined);
+  // Get the first record
+  state.data.record = record
+   return state;
+});
+
 sqlString(state => {
   console.log(state);
 })
