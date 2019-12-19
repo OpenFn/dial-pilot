d05452727d352a682952e19c3c1d28839ac79bea
Taylor Downs <taylor@openfn.org>
2019-12-19 12:57:37 -0700

    sconrad@digitalimpactalliance.org's change on OpenFn.org

--- jobs/03-update-payment-status.js
+++ jobs/03-update-payment-status.js
@@ -7,17 +7,18 @@ alterState(state => {
   // Note: we pluck out on the 'RowDataResponses' from the SQL server.
   const recordData = state.response.body.filter(x => x.record !== undefined);
   // Get the first record
-  console.log(recordData);
   state.data.record = recordData[0] && recordData[0].record;
   return state;
 });
 
 sqlString(state => {
-  console.log(state.data.body);
   return `UPDATE entry SET string_value='completed' where record=${state.data.record} and string_value='initiated'`;
 });
 
 sqlString(state => {
-  console.log(state.data.body);
   return `UPDATE last_entry SET string_value='completed' where record=${state.data.record} and string_value='initiated'`;
+});
+
+sqlString(state => {
+  return `UPDATE hippo_person_payments SET status='completed' where id='person_payments|${state.data.record}'`;
 });
