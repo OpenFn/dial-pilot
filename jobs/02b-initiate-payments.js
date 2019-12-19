ea6aa02ddf76b1e3d7f4e9ff35e5804d0023c3d2
Taylor Downs <taylor@openfn.org>
2019-12-19 09:22:11 -0500

    sconrad@digitalimpactalliance.org's change on OpenFn.org

--- jobs/02b-initiate-payments.js
+++ jobs/02b-initiate-payments.js
@@ -78,10 +78,8 @@ each(
         'form[person_payments][0][0][fields][transactionId]':
           state.data.body.transactionId,
         'form[person_payments][0][0][fields][status]':
-          state.data.statusCode == 200 ? 'initiated' : 'failed',
+          state.data.body.transactionId ? 'initiated' : 'failed',
       };
-      console.log(state.data.person_payment);
-      console.log(state.data.statusCode);
       // =====================================================================
       // Create "initiated" payments in iHRIS with their mifos external IDs ==
       post(
