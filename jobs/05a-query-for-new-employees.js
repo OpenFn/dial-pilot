90e693edb961e2db96a0d892352a488701f27c8f
Taylor Downs <taylor@openfn.org>
2019-12-20 07:25:31 -0700

    sconrad@digitalimpactalliance.org's change on OpenFn.org

--- jobs/05a-query-for-new-employees.js
+++ jobs/05a-query-for-new-employees.js
@@ -2,6 +2,11 @@ sqlString(state => {
   return "SELECT person_id, person_name, msisdn, status FROM employee_updates WHERE status='NEW';";
 });
 
+alterState(state => {
+  state.new_employees = state.response.body;
+  console.log(state.new_employees);
+})
+
 sqlString(state => {
   return `UPDATE employee_updates SET status='PROCESSED' WHERE status='NEW'`;
 });
