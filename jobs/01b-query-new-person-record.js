4e3197621765893f3f2d4938f11c818884142226
Taylor Downs <taylor@openfn.org>
2019-12-20 13:46:57 -0700

    sconrad@digitalimpactalliance.org's change on OpenFn.org

--- jobs/01b-query-new-person-record.js
+++ jobs/01b-query-new-person-record.js
@@ -13,7 +13,7 @@ sqlString(state => {
 
 alterState(state => {
   // Note: we pluck out on the 'RowDataResponses' from the SQL server.
-  const personData = state.response.body.filter(x => x.id !== undefined);
+  const personData = state.response.body;
   // Get the first record
   state.data.person_id = personData[0] && personData[0].id
    return state;
